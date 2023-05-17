23492de55751f1c43e4c3d1beda49a5497e0c54a
labrac <raquelortegalabrac@hotmail.com>
2023-05-17 20:24:31 +0200

    Add PetsList class

--- src/components/pets.list.ts
+++ src/components/pets.list.ts
@@ -8,13 +8,30 @@ export class PetsList extends Component {
   constructor(selector: string) {
     super(selector);
     this.render();
+    console.log(this.element);
     console.log(this.pets);
   }
 
   render() {
     super.cleanHtml(this.selector);
-    this.template = 
+    this.template = this.createTemplate();
+  }
+
+  createTemplate() {
+    const list = this.pets
+      .map(
+        (item) => `
+        <li>
+          <span>${item.id}</span>
+          <span>${item.name}</span>
+          <span>${item.breed}</span>
+          <span>${item.isAdopted}</span>
+          <span>${item.owner}</span>
+        </li>
+        `
+      )
+      .join("");
+
+    return `<ul>${list}</ul>`;
   }
 }
-
-
