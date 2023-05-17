5d09c65bde48aebcbac789ffe0ae29aaf24f5a9f
labrac <raquelortegalabrac@hotmail.com>
2023-05-17 20:54:42 +0200

    Add buttons and import mock to pets.list.ts

--- src/components/pets.list.ts
+++ src/components/pets.list.ts
@@ -1,3 +1,4 @@
+import { getMockPets } from "../data/mock.pets";
 import { Pet } from "../models/pets";
 import { Component } from "./component";
 
@@ -7,6 +8,8 @@ export class PetsList extends Component {
   pets: Pet[];
   constructor(selector: string) {
     super(selector);
+    this.pets = getMockPets();
+    this.pets[0].isAdopted = true;
     this.render();
     console.log(this.element);
     console.log(this.pets);
@@ -15,6 +18,20 @@ export class PetsList extends Component {
   render() {
     super.cleanHtml(this.selector);
     this.template = this.createTemplate();
+    const element = super.render();
+    document
+      .querySelectorAll(".button")
+      .forEach((item) =>
+        item.addEventListener("click", this.handleDelete.bind(this))
+      );
+    return element;
+  }
+
+  handleDelete(event: Event) {
+    const element = event.target as HTMLSpanElement;
+    this.pets = this.pets.filter((item) => item.id !== element.dataset.id);
+    console.log(this.pets);
+    this.render();
   }
 
   createTemplate() {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { Main } from "./components/main";
+import { PetsList } from "./components/pets.list";
 
 new Main("#app");
+new PetsList("main");
